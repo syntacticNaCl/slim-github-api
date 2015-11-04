09f57f750ff34a88e75b8795c989980800bfe287
Garrett Rappaport <garrettrappaport@gmail.com>
2015-11-04 01:38:36 -0500

    Added user input for username. Moved username call to GH API outside of routes. Grouped API routes.

--- index.php
+++ index.php
@@ -5,48 +5,86 @@ use GuzzleHttp\Client;
 
 $app = new \Slim\Slim();
 
-$app->get('/', function(){
-  echo "Welcome to the Slim Based Github API";
-});
-
-// print out repos for user provided
-$app->get('/api/:username', function ($username) {
+/**
+ * Return Github.com user JSON object
+ * @param  string $username Github.com username
+ * @return object User object
+ */
+function getUserRepos ($username) {
   $client = new Client([
-      'base_uri' => 'https://api.github.com/'
-  ]);
+    'base_uri' => 'https://api.github.com/'
+    ]);
 
   // get user repos
   $response = $client->request("GET", "users/$username/repos");
 
-  $repos = json_decode($response->getBody());
+  return json_decode($response->getBody());
+  
+};
 
-  echo "Current repos for user $username:" . '<br><br>';
+$app->get('/', function() use ($app) {
+  echo "Welcome to the Slim Based Github API";
+  $app->render('form.php');
+
+});
+
+$app->post('/', function() use ($app) {
+
+  $req = $app->request();
+  $uname = $req->params('uname');
+  
+  $repos = getUserRepos($uname);
+  
+
+  echo 'Current repos for user ' . $uname . ':<br>';
 
   foreach ($repos as $repo) {
-    echo $repo->name . '<br>';
+    echo '<a href="/api/' . $uname . '/' . $repo->name . '">' . $repo->name . '</a><br>';
   }
+
+  echo '<br><br><a href="/">Search Again</a>';
 
 });
 
-// print out commits from the provided repo
-$app->get('/api/:username/:repo', function($username,$repo){
-  $client = new Client([
+// group for api; preparing for when more methods are added
+$app->group('/api',function() use ($app) {
+  // print out repos for user provided
+  $app->get('/:username', function ($username) {
+
+    $repos = getUserRepos($username);
+
+    echo 'Current repos for user ' . $username . ':<br>';
+
+    foreach ($repos as $repo) {
+      echo '<a href="/api/' . $username . '/' . $repo->name . '">' . $repo->name . '</a><br>';
+    }
+
+    echo '<br><br><a href="/">Search Again</a>';
+
+  });
+
+  // print out commits from the provided repo
+  $app->get('/:username/:repo', function($username,$repo){
+    $client = new Client([
       'base_uri' => 'https://api.github.com/'
-  ]);
+      ]);
 
-  // get user commits
-  $response = $client->request("GET", "repos/$username/$repo/commits");
+    // get user commits
+    $response = $client->request("GET", "repos/$username/$repo/commits");
 
-  $commits = json_decode($response->getBody());
+    $commits = json_decode($response->getBody());
 
-  echo "Commits for the $repo repo by user $username:" . '<br><br>';
+    echo "Commits for the $repo repo by user $username:" . '<br><br>';
 
-  foreach ($commits as $commit) {
-    echo $commit->commit->message . '<br>';
-  }
+    foreach ($commits as $commit) {
+      echo $commit->commit->message . '<br>';
+    }
 
+    echo '<br><br><a href="/api/' . $username . '">Back to Repos</a>';
+
+    echo '<br><br><a href="/">Start Over</a>';
+
+  });
 });
-
-// TODO add POST so user has a form to enter in their username and/or repo
 
 $app->run();
